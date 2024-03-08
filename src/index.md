d6962db24d1e7159c384b35a744b04abd3664d49
WalkableSun <jobschopman@icloud.com>
2024-03-08 01:50:37 +0100

    api added

--- src/index.md
+++ src/index.md
@@ -3,13 +3,16 @@ layout: layout.html
 pageTitle: New York Today
 tags: page
 navTitle: Home
+pageClass: Home
 ---
 
 ## My Articles
 
-<button>Articles</button>
+<!-- <button>Articles</button> -->
 
-{% for page in collections.page %}
+<main class="stories">loading...</main>
+
+<!-- {% for page in collections.page %}
   <h2><a href="{{ page.url | url }}">{{ page.data.pageTitle }}</a></h2>
   <em>{{ page.date | date: "%Y-%m-%d" }}</em>
-{% endfor %}
+{% endfor %} -->
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,58 @@
-document.addEventListener("click", clickHandlers);
+// document.addEventListener("click", clickHandlers);
 
-function clickHandlers(event) {
-  if (!event.target.matches("button")) return;
-  fetch("https://jsonplaceholder.typicode.com/posts")
+// store the link plus the API key in a variable
+const api =
+  "https://api.nytimes.com/svc/topstories/v2/nyregion.json?api-key=I1v92ZQoxXVhltaR4Di8ZiNndFaWe16C";
+
+function getStories() {
+  // if (!event.target.matches("button")) return;
+  fetch(api)
     .then((response) => response.json())
-    .then((json) => console.log(json));
+    .then((data) => showData(data.results));
 }
+
+function showData(stories) {
+  var looped = stories
+    .map((story) => `
+    <div class="item">
+    <picture>
+    <img src="${story.multimedia[2].url}" alt="" />
+    <caption>${story.multimedia[2].caption}</caption>
+    </picture>
+      <h3><a href="${story.url}">${story.title}</a></h3>
+      <p>${story.abstract}</p>
+    </div>
+  `
+    )
+    .join("");
+
+  document.querySelector(".stories").innerHTML = looped;
+}
+
+if(document.querySelector('.home')){
+  getStories()
+}
+
+
+// function clickHandlers(event) {
+//   if (!event.target.matches("button")) return;
+//   fetch("https://jsonplaceholder.typicode.com/posts")
+//     .then((response) => response.json())
+//     .then(function (data){
+//       showData(data);
+//     });
+// }
+
+// function showData(data) {
+
+//   // for (let post of data) {
+//   //   content += `<h3>${post.title}</h3><p>${post.body}</p>`; // new
+//   // }
+//     let content = data.map((post) => `
+//     <h3>${post.title}</h3>
+//     <p>${post.body}</p>
+//     `
+//     ).join("");
+  
+//     document.querySelector(".stories").innerHTML = content;
+  // }
